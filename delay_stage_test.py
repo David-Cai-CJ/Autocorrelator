5248e11529c2301d4927cc079fa97a037fc224ca
David-Cai-CJ <jiajun.cai@mail.mcgill.ca>
2023-02-09 15:46:48 -0500

    wrap up thursday

--- delay_stage_test.py
+++ delay_stage_test.py
@@ -4,8 +4,8 @@ from time import sleep
 
 xps = XPSController()
 
-stage = xps.delay_stage
-stage.absolute_move(12.5)
+stage = xps.autocorr_stage
+stage.absolute_move(5.5)
 
 min_move = stage.min_limit
 max_move = stage.max_limit
@@ -13,9 +13,9 @@ max_move = stage.max_limit
 
 while True:
     try:
-        stage.relative_move(.5)
+        stage.relative_move(2)
         sleep(2)
-        stage.relative_move(-.5)
+        stage.relative_move(2)
         sleep(2)
     except KeyboardInterrupt:
         break
--- test_run.py
+++ test_run.py
@@ -3,7 +3,7 @@ from moku.instruments import Oscilloscope
 
 from uedinst.delay_stage import XPSController
 from time import sleep
-
+import os
 
 
 moku_address = '[fe80:0000:0000:0000:7269:79ff:feb9:1a40%9]'
@@ -14,24 +14,14 @@ osc = Oscilloscope(moku_address, force_connect=True)
 print('done')
 dLogger = Datalogger(moku_address, force_connect=True)
 # https://apis.liquidinstruments.com/reference/oscilloscope/
-xps = XPSController()
+xps = XPSController(reset = False)
 
-
+with open('D:\autocorr_log\myOutput.txt', 'wb') as f:
+    f.write('')
 # stage = xps.autocorr_stage
-# stage.absolute_move((min_move+max_move)/2)
 # min_move = stage.min_limit
 # max_move = stage.max_limit
 
-
-
-# while True:
-#     try:
-#         stage.absolute_move(min_move)
-#         sleep(2)
-#         stage.absolute_move(max_move)
-#         sleep(2)
-#     except KeyboardInterrupt:
-#         break
 
 # ###
 # PEAK_POS_MM = 22.652
@@ -44,20 +34,27 @@ xps = XPSController()
 # MIN_POS_MM = PEAK_POS_MM - RANGE_MM
 
 
-# # # Set data logger to osc
-# # dLogger.set_frontend(channel=2, impedance='1MOhm',
-# #                      coupling="DC", range="5Vpp")
-# # # Log 100 samples per second
-# # dLogger.set_samplerate(1000)
-# # dLogger.set_acquisition_mode(mode='Precision')
+# # Set data logger to osc
+# dLogger.set_frontend(channel=2, impedance='1MOhm',
+#                      coupling="DC", range="10Vpp")
+# # Log 100 samples per second
+# dLogger.set_samplerate(1000)
+# dLogger.set_acquisition_mode(mode='Precision')
 
-# # print("Logger setup done.")
+# print("Logger setup done.")
 
 # ###
 
-# print(f"Moving stage to {MIN_POS_MM}")
-# stage.absolute_move(MIN_POS_MM)
-# print(f"Stage moved to {MIN_POS_MM}")
+# print(f"Moving stage to {PEAK_POS_MM}")
+# stage.absolute_move(PEAK_POS_MM)
+# print(f"Stage moved to {PEAK_POS_MM}")
+
+
+# logFile = dLogger.start_logging(duration=10,
+#                                     file_name_prefix=str(stage.current_position()).replace(".", "_"))
+
+
+# dLogger.download("persist", logFile, "D:/autocorr_log")
 
 # # while stage.current_position() < MAX_POS_MM:
 # #     logFile = dLogger.start_logging(duration=10,
@@ -77,4 +74,4 @@ xps = XPSController()
 
 # #     stage.relative_move(STEP_SIZE_MM)
 
-# # dLogger.relinquish_ownership()
+# dLogger.relinquish_ownership()
