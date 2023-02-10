2889444c51ee231d88489933dc5c9efb9397dd67
David-Cai-CJ <jiajun.cai@mail.mcgill.ca>
2023-02-10 16:56:51 -0500

    wrap up for weekend

--- readme.md
+++ readme.md
@@ -11,3 +11,9 @@ https://github.com/Siwick-Research-Group/uedinst
   - move ~ 100 fs (user set) before the peak, scan across in 100 nm steps. Plotting in plt.
 
 NOTE: With `uedinst` under development, I can work on both this project and request pull to `uedinst` by installing `uedinst` with `python setup.py develop`
+
+
+sys:1: ResourceWarning: unclosed <socket.socket fd=1180, family=AddressFamily.AF_INET6, type=SocketKind.SOCK_STREAM, proto=0, laddr=('fe80::5028:c5cc:a2ae:1ae4%9', 3409, 0, 9), raddr=('fe80::7269:79ff:feb9:1a40%9', 80, 0, 9)>
+sys:1: ResourceWarning: unclosed <socket.socket fd=1188, family=AddressFamily.AF_INET6, type=SocketKind.SOCK_STREAM, proto=0, laddr=('fe80::5028:c5cc:a2ae:1ae4%9', 3415, 0, 9), raddr=('fe80::7269:79ff:feb9:1a40%9', 80, 0, 9)>
+
+have to figure out how to close socket connections
--- scan.py
+++ scan.py
@@ -1,35 +1,42 @@
 from moku.instruments import Datalogger
 from moku.instruments import Oscilloscope
-
 from uedinst.delay_stage import XPSController
 from time import sleep
 import os
+import numpy as np
+import socket
 
 
 moku_address = '[fe80:0000:0000:0000:7269:79ff:feb9:1a40%9]'
 
+# If it says API already connected, close it from fd in the WARNING message. (Probably better ways...)
+
+# socket.socket().close(404)
+# socket.socket().close(1192)
+
 
 osc = Oscilloscope(moku_address, force_connect=True)
-print('done')
 dLogger = Datalogger(moku_address, force_connect=True)
+print('Established Moku connections')
 # https://apis.liquidinstruments.com/reference/oscilloscope/
 xps = XPSController(reset=False)
 
-
 stage = xps.autocorr_stage
+
+### hardware limits
 min_move = stage.min_limit
 max_move = stage.max_limit
 
 
-###
+### signal limits for ~100fs pulse
 PEAK_POS_MM = 22.652
 RANGE_PS = .2
-RANGE_MM = stage.delay_to_distance(RANGE_PS)
+RANGE_MM = abs(stage.delay_to_distance(RANGE_PS))
 STEP_SIZE_MM = 100e-6  # 100 nm step size
 
 # Range of motion
-MAX_POS_MM = PEAK_POS_MM + RANGE_MM
-MIN_POS_MM = PEAK_POS_MM - RANGE_MM
+MAX_POS_MM = round(PEAK_POS_MM + RANGE_MM,4)
+MIN_POS_MM = round(PEAK_POS_MM - RANGE_MM,4)
 
 
 # Set data logger to osc
@@ -39,20 +46,22 @@ dLogger.set_frontend(channel=2, impedance='1MOhm',
 dLogger.set_samplerate(1000)
 dLogger.set_acquisition_mode(mode='Precision')
 
-print("Logger setup done.")
 
 ###
 
-print(f"Moving stage to {PEAK_POS_MM}")
-stage.absolute_move(PEAK_POS_MM)
-print(f"Stage moved to {PEAK_POS_MM}")
+print(f"Moving stage to {MIN_POS_MM}")
+stage.absolute_move(MIN_POS_MM)
+print(f"Stage moved to {stage.current_position()}")
 
 filenames = []
 
 
-while stage.current_position() < MAX_POS_MM:
-    logFile = dLogger.start_logging(duration=10,
-                                    file_name_prefix=str(stage.current_position()).replace(".", "_"))
+while stage.current_position() <= MAX_POS_MM:
+    
+    prefix = str(stage.current_position()).replace(".", "_")
+    print(prefix.rjust(30), np.mean(osc.get_data()['ch2']))
+    logFile = dLogger.start_logging(duration=1,
+                                    file_name_prefix=prefix)
 
     is_logging = True
 
@@ -73,6 +82,7 @@ while stage.current_position() < MAX_POS_MM:
 for fname in filenames:
     dLogger.download("persist",
                      fname,
-                     './log' + os.sep + fname)
+                     './logging' + os.sep + fname)
 
 dLogger.relinquish_ownership()
+osc.relinquish_ownership()
