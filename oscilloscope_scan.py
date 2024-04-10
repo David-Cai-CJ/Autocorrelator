d06481d2dacbce4126f1b2092e24dffce778006c
David Cai <jiajun.cai@mail.mcgill.ca>
2024-04-10 12:38:21 -0400

    ready to test in lab

--- oscilloscope_scan.py
+++ oscilloscope_scan.py
@@ -1,5 +1,5 @@
-# from moku.instruments import Oscilloscope
-# from uedinst.delay_stage import XPSController
+from moku.instruments import Oscilloscope
+from uedinst.delay_stage import XPSController
 import os
 import numpy as np
 import socket
@@ -29,82 +29,82 @@ except FileExistsError:
 file_dir = path + os.path.sep + 'data.hdf5'
 
 # #########
-# moku_address = '172.25.12.13' 
+moku_address = '172.25.12.13' 
 
-# ## If it says API already connected, close it from fd in the WARNING message. (Probably better ways...)
-# # socket.socket().close(404)
-# # socket.socket().close(1192)
+## If it says API already connected, close it from fd in the WARNING message. (Probably better ways...)
+# socket.socket().close(404)
+# socket.socket().close(1192)
 
-# osc = Oscilloscope(moku_address, force_connect=True)
-# # osc.osc_measurement(-1e-6, 3e-6,"Input2",'Rising', 0.04)
-# osc.set_source(2, source='Input2')
-# osc.set_acquisition_mode(mode='Precision')
-# osc.set_trigger(auto_sensitivity=False, hf_reject=False,
-#                 noise_reject=False, mode='Normal', level=0.3, source='Input2')
-# osc.set_timebase(-0.5e-6, 3e-6)
-# # https://apis.liquidinstruments.com/reference/oscilloscope/
+osc = Oscilloscope(moku_address, force_connect=True)
+# osc.osc_measurement(-1e-6, 3e-6,"Input2",'Rising', 0.04)
+osc.set_source(2, source='Input2')
+osc.set_acquisition_mode(mode='Precision')
+osc.set_trigger(auto_sensitivity=False, hf_reject=False,
+                noise_reject=False, mode='Normal', level=0.3, source='Input2')
+osc.set_timebase(-0.5e-6, 3e-6)
+# https://apis.liquidinstruments.com/reference/oscilloscope/
 
-# # reset=False will not reset the stages to factory default locations.
-# xps = XPSController(reset=False)
+# reset=False will not reset the stages to factory default locations.
+xps = XPSController(reset=False)
 
-# print("Hardwares connected")
-# stage = xps.autocorr_stage
+print("Hardwares connected")
+stage = xps.autocorr_stage
 
-# # hardware limits
-# min_move = stage.min_limit
-# max_move = stage.max_limit
+# hardware limits
+min_move = stage.min_limit
+max_move = stage.max_limit
 
-# # signal limits for ~100fs pulse
+# signal limits for ~100fs pulse
 
 PEAK_POS_MM = 11.136
 
-# # 0.03 mm per 100 fs
-# RANGE_MM = 0.10
-# STEP_SIZE_MM = 2e-3
+# 0.03 mm per 100 fs
+RANGE_MM = 0.10
+STEP_SIZE_MM = 2e-3
 
-# MAX_POS_MM = round(PEAK_POS_MM + RANGE_MM, 4)
-# MIN_POS_MM = round(PEAK_POS_MM - RANGE_MM, 4)
-
-
-# ########
-# print("Start Scanning\n\n")
-
-# pos = np.round(np.arange(MIN_POS_MM, MAX_POS_MM +
-#                STEP_SIZE_MM, STEP_SIZE_MM), 4)
-
-# with h5py.File(file_dir, 'a') as hf:
-#     hf.create_dataset("positions", data = pos)
-
-# ########
-# trange = tqdm.tqdm(pos)
+MAX_POS_MM = round(PEAK_POS_MM + RANGE_MM, 4)
+MIN_POS_MM = round(PEAK_POS_MM - RANGE_MM, 4)
 
 
-# fig, ax = plt.subplots(1, 1)
+########
+print("Start Scanning\n\n")
 
-# v_arr = []
+pos = np.round(np.arange(MIN_POS_MM, MAX_POS_MM +
+               STEP_SIZE_MM, STEP_SIZE_MM), 4)
 
-# for loc in trange:    
-#     stage.absolute_move(loc)
-#     trange.set_postfix({'Position': f'{loc}'})
+with h5py.File(file_dir, 'a') as hf:
+    hf.create_dataset("positions", data = pos)
 
-#     v_loc = np.zeros(n_samples)
+########
+trange = tqdm.tqdm(pos)
 
-#     with h5py.File(file_dir, 'a') as hf:
-#         grp = hf.create_group(f"{loc}".replace(".", "_"))
 
-#         for n in np.arange(n_samples): 
-#             measurement = osc.get_data()
-#             t = measurement['time']
-#             v = measurement['voltage']
-#             subgrp = grp.create_group(f'{n}') 
-#             subgrp.create_dataset('time', data = t)
-#             subgrp.create_dataset('voltage', data =v)
+fig, ax = plt.subplots(1, 1)
+
+v_arr = []
+
+for loc in trange:    
+    stage.absolute_move(loc)
+    trange.set_postfix({'Position': f'{loc}'})
+
+    v_loc = np.zeros(n_samples)
+
+    with h5py.File(file_dir, 'a') as hf:
+        grp = hf.create_group(f"{loc}".replace(".", "_"))
+
+        for n in np.arange(n_samples): 
+            measurement = osc.get_data()
+            t = measurement['time']
+            v = measurement['voltage']
+            subgrp = grp.create_group(f'{n}') 
+            subgrp.create_dataset('time', data = t)
+            subgrp.create_dataset('voltage', data =v)
             
-#             v_loc[n] = np.sum(v)
-#     v_arr.append(np.mean(v_loc))
+            v_loc[n] = np.sum(v)
+    v_arr.append(np.mean(v_loc))
 
-#     ax.plot(pos[:len(v_arr)], v_arr)
-#     plt.pause(.1)
+    ax.plot(pos[:len(v_arr)], v_arr)
+    plt.pause(.1)
 
 
 stage.absolute_move(PEAK_POS_MM + 5*RANGE_MM)
@@ -135,6 +135,25 @@ for p in pos_mm:
 v_arr = np.array(v_arr) / np.max(v_arr)   # Normalize
 
 
+
+# Quick and dirty...
+
+left_found = False
+right_found = False
+normed = v_arr - np.min(v_arr)
+normed /= np.max(normed)
+
+for i, val in enumerate(normed - 0.5):
+    if val > 0 and not left_found:
+        left = i
+        left_found = True
+    if left_found and val < 0 and not right_found:
+        right = i
+        right_found = True
+
+# print(left, right)
+print("FWHM quick: ".ljust(15) +  f"{(pos_mm[right] - pos_mm[left])/1e3/2.998e8/1e-15*2:.2f} fs")
+
 # fitting
 def gau(x, x0, s):
     return 1/np.sqrt(2/np.pi)*np.exp(-1/2*(x-x0)**2/s**2)
@@ -143,31 +162,12 @@ model = lambda x,A,x0,s,C: A*gau(x,x0,s) + C
 
 p0 = [2.5, 11.136,0.04, 0.]
 
-fit, _ = curve_fit( model, pos_mm, v_arr, p0=p0,
+fit, _ = curve_fit( model, pos_mm, normed, p0=p0,
                      bounds=([.001, p0[1] - .1,  .001, p0[-1] - 0.5],
                              [40, p0[1] + .1,  .07, p0[-1] + 1]))
 
 
-
 A, x0, s, C = fit
-
-
-# Quick and dirty...
-
-# left_found = False
-# right_found = False
-# normed = v_arr - np.min(v_arr)
-# normed /= np.max(normed)
-# for i, val in enumerate(normed - 0.5):
-#     if val > 0 and not left_found:
-#         left = i
-#         left_found = True
-#     if left_found and val < 0 and not right_found:
-#         right = i
-#         right_found = True
-
-# print(left, right)
-# print((pos_mm[right] - pos_mm[left])/1e3/2.998e8/1e-15*2)
 
 
 ## conversions
@@ -178,23 +178,25 @@ width = s /1e3/3e8 / 1e-15 * 2
 
 ## plotting and saving
 
-print('sigma is'.rjust(15) + f'{width:.2f}')
+print('FWHM fit: '.ljust(15) + f'{fwhm_factor * width:.2f} fs')
 
 # Plotting
 
 
 f, ax =  plt.subplots(1,1)
-ax.plot(pos_mm, v_arr, 'k.',lw=.5, ms = 3, alpha = .7, zorder= -1, markevery=1, label='Data')
-# ax.plot(t_fs, model(pos_mm, *fit), c='b', lw=1,  label='Fit')
-# ax.plot([],[], lw=1, c="b", label=f"FWHM={width*fwhm_factor:.2f} fs")
-# ax.axvline(t_fs[left], c='r')
-# ax.axvline(t_fs[right], c='r')
-# ax.axhline(0.5, c='r', label=f"FWHM={(pos_mm[right] - pos_mm[left])/1e3/2.998e8/1e-15*2:.2f} fs")
+ax.plot(t_fs, normed, 'k.',lw=.5, ms = 3, alpha = .7, zorder= -1, markevery=1, label='Data')
+ax.plot(t_fs, model(pos_mm, *fit), c='b', lw=1, )
+ax.plot([],[], lw=1, c="b", label=f"FWHM={width*fwhm_factor:.2f} fs")
+ax.axvline(t_fs[left], c='r', ls = '--', lw =.7, alpha = .7)
+ax.axvline(t_fs[right], c='r',  ls = '--', lw =.7, alpha = .7)
+ax.axhline(0.5, c='r',  ls = '--', lw =.7, alpha = .7, 
+           label=f"FWHM={(pos_mm[right] - pos_mm[left])/1e3/2.998e8/1e-15*2:.2f} fs")
 
-# np.savetxt(path + os.path.sep + "times.txt" , t_fs)
-# np.savetxt(path + os.path.sep + "normed_intensity.txt", normed)
+np.savetxt(path + os.path.sep + "times.txt" , t_fs)
+np.savetxt(path + os.path.sep + "normed_intensity.txt", normed)
 
 ax.set_xlabel("Delay [fs]")
+ax.set_ylabel("Relative Intensity")
 ax.legend()
 f.tight_layout()
 
